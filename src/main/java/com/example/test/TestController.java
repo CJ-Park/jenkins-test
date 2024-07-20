78ff9c0ab2173452a905b5f0c23b0ec05eb6e237
CJ-Park <king01286@naver.com>
2024-07-20 23:40:44 +0900

    git push for jenkins trigger test

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hi")
     public String test() {
-        return "hi~";
+        return "jenkins trigger test";
     }
 }
